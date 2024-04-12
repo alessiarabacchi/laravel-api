e8f464ab9f5624bb340da3152bd43f31612eb2e8
alessiarabacchi <alessia.rabacchi@icloud.com>
2024-04-12 15:36:58 +0200

    create component projectcard

---
+++ vite-boolfolio/src/components/ProjectCard.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    props: {
+        project: Object,
+    },
+};
+</script>
+
+<template>
+    <div class="project-card">
+        <h3>{{ project.name }}</h3>
+        <p>{{ project.description }}</p>
+    </div>
+</template>
